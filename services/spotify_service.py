e4b329f52101b20c449834eea3b04076f9bcb50f
sarthak1510 <85314201+sarthak1510@users.noreply.github.com>
2025-07-15 01:25:12 +0530

    Update spotify_service.py
    changes

--- services/spotify_service.py
+++ services/spotify_service.py
@@ -1,25 +1,29 @@
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyClientCredentials
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 class SpotifyService:
-    def __init__(self):
-        auth = SpotifyClientCredentials(
-            client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-            client_secret=os.getenv("SPOTIPY_CLIENT_SECRET")
-        )
-        self.client = Spotify(auth_manager=auth)
+    def __init__(self, client):
+        self.client = client
 
-    def search_tracks(self, query="pop", limit=5):
+    def search_tracks(self, query, limit=10):
+        """
+        Search for tracks on Spotify given a query string.
+        Ensures all returned tracks have the required metadata.
+        """
         results = self.client.search(q=query, type="track", limit=limit)
         tracks = []
-        for t in results["tracks"]["items"]:
-            tracks.append({
-                "name": t["name"],
-                "artist": t["artists"][0]["name"],
-                "explicit": t["explicit"]
-            })
+
+        for item in results['tracks']['items']:
+            track_id = item.get('id')
+            name = item.get('name')
+            artist = item['artists'][0]['name'] if item.get('artists') else "Unknown"
+            explicit = item.get('explicit', False)
+            spotify_url = item.get('external_urls', {}).get('spotify', '') 
+
+            if track_id and name:
+                tracks.append({
+                    "id": track_id,
+                    "name": name,
+                    "artist": artist,
+                    "explicit": explicit,
+                    "spotify_url": spotify_url  
+                })
+
         return tracks
